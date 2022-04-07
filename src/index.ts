329938c81834f7e445198c065d325a650426dac3
domingo2000 <domingo.edwards@uc.cl>
2022-04-07 13:56:30 -0400

    ft-basic-app

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+
+import logger from 'koa-logger';
+import json from 'koa-json';
+
+import koaBody from 'koa-body';
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', async (ctx, next) => {
+  ctx.body = 'Hello World';
+  await next();
+};
+
+app.use(json());
+app.use(logger());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
